3a4579d6900b57270b680e9ea9f3d38979eefc95
Robert Jaremczak <rjaremczak@gmail.com>
2017-09-30 23:41:27 +0200

    cmake file refactored

--- lib/io/InOut.h
+++ lib/io/InOut.h
@@ -5,17 +5,18 @@
 #ifndef RADIO3_STM32_INOUT_H
 #define RADIO3_STM32_INOUT_H
 
+#include <cstdint>
 
 class InOut {
     enum Status {
-        OK, INCOMPLETE, TIMEOUT;
+        OK, INCOMPLETE, TIMEOUT
     };
 
 public:
     virtual uint8_t available() = 0;
     virtual Status read(uint8_t *buf, uint16_t size) = 0;
-    virtual Status write(const uint8_t *buf, size) = 0;
-    virtual bool canWrite()
+    virtual Status write(const uint8_t *buf, uint16_t size) = 0;
+    virtual bool idle();
 };
 
 
